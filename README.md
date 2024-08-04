9552dc164de451d0d5093310bda3b0beafa64b86
Manuel Denis <manueldenis.developer@gmail.com>
2024-08-04 15:34:59 +0200

    Finalizado Laboratorio 3 - Variables

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-# typescript-sandbox
+# BootcampJS2-Modulo-02
 
-Para la parte de cada módulo en la que te planteamos ejercicios con JavaScript puedes usar este sandbox.
+## Modulo 2
 
-Pasos:
+### Ejercicio de laboratorio de Manuel Denis.
 
-- Clonate el proyecto.
-- Instala las dependencias con `npm install`.
-- Ejecuta el sandbox con `npm run dev`.
-- Abre el navegador en `http://localhost:5173/` (si ese puerto no te funciona, mira en la consola donde has hecho el build, puede que este ocupado y se haya abierto en otro puerto).
-- Puedes empezar a meter tu código en el fichero:
+#### Laboratorio Variables.
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,73 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+/*
+
+- Nombre del grupo
+- Año de fundacion
+- Si esta en activo
+- Estilo musical
+
+The Beatles - 1960 - No - 🎵 Pop Rock
+Queen - 1970 - No - 🎸 Rock
+Ac/Dc - 1973 - Si - 🤘 Hard Rock
+Ludwig van Beethoven - 1770 - No - 🎼 Clasica
+The Rolling Stones - 1962 - Si - 🎸 Rock
+
+*/
+
+const estiloNombre =
+  "font-weight: bold; background-color: #0f0; color: #000; padding: 5px; font-size: 20px;";
+
+interface Grupo {
+  nombre: string;
+  año: number;
+  activo: boolean;
+  estilo: string;
+}
+
+const grupo1: Grupo = {
+  nombre: "The Beatles",
+  año: 1960,
+  activo: false,
+  estilo: "🎵 Pop Rock",
+};
+
+const grupo2: Grupo = {
+  nombre: "Queen",
+  año: 1970,
+  activo: false,
+  estilo: "🎸 Rock",
+};
+
+const grupo3: Grupo = {
+  nombre: "Ac/Dc",
+  año: 1973,
+  activo: true,
+  estilo: "🤘 Hard Rock",
+};
+
+const grupo4: Grupo = {
+  nombre: "Ludwig van Beethoven",
+  año: 1770,
+  activo: false,
+  estilo: "🎼 Clasica",
+};
+
+const grupo5: Grupo = {
+  nombre: "The Rolling Stones",
+  año: 1962,
+  activo: true,
+  estilo: "🎸 Rock",
+};
+
+console.log(grupo1);
+console.log(grupo2);
+console.log(grupo3);
+console.log(grupo4);
+console.log(grupo5);
+
+console.log(`%c${grupo1.nombre}`, estiloNombre);
+console.log(`%c${grupo2.nombre}`, estiloNombre);
+console.log(`%c${grupo3.nombre}`, estiloNombre);
+console.log(`%c${grupo4.nombre}`, estiloNombre);
+console.log(`%c${grupo5.nombre}`, estiloNombre);
